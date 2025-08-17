bf5a661a256f799765ed8feb09a66aa60c86e0c1
EnLiao <pikachu200594@gmail.com>
2025-08-18 03:44:48 +0800

    feat: add .nojekyll

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', 
-  basePath: '',     
   images: {
     unoptimized: true, 
   },
